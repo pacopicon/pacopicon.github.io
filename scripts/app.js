476fce1a071e384e9eaa36fd03dc75d575c71030
Paco <palmtreerooskee@gmail.com>
2016-04-23 20:54:03 -0500

    tweaking mobile responsiveness 11: tweaking the hamburgerMenu listener

--- scripts/app.js
+++ scripts/app.js
@@ -117,13 +117,13 @@ $(document).ready(function() {
     // Hamburger & x-out listeners
     $(".hamburger").click(function () {  
         $("#hamburgerMenu").animate({right: "+=20%"});
-        $(".content").animate({width: "-=20%"})
+        $(".content").animate({width: "-=15%"})
         $(".hamburger").hide();    
     });
     
     $(".x-out").click(function () {  
         $("#hamburgerMenu").animate({right: "-20%"});
-        $(".content").animate({width: "+=20%"})
+        $(".content").animate({width: "+=15%"})
         $(".hamburger").show();
     });
     
